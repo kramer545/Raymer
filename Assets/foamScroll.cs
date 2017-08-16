3758305a86d593a634e2253e456af8b097fbc0be
Ryan Kramer <kramer545@gmail.com>
2017-08-15 20:45:01 -0700

    messed with foam, splashing and fog

---
+++ Assets/foamScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foamScroll : MonoBehaviour {
+
+    public float scrollSpeed;
+    float offset = 0;
+    Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset += Time.deltaTime * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(-offset,offset));
+	}
+}
