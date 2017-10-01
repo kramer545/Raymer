7fd4d9c41842d9d062e3d2ed5ff40efebcbb2d75
Ryan Kramer <kramer545@gmail.com>
2017-09-30 20:31:32 -0700

    repalced waypoints w/ anims, added fish eating and swim around anim, worked on dock scene

---
+++ Assets/lakeScene/DockArea/DocksController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DocksController : MonoBehaviour {
+
+	/*This script handles the dock area, where the user is in the fov of a fish, who cant cross under the rock covered docks
+	Then after clicking the UI btn, this sends the cam to a new waypoint at end of dock, where they are eaten by a fish
+	After this, the rocks are *cleared* and fish/cam goes under docks, before the ui goes back to main cam
+	*/
+
+	public bool rocksExist;
+	public GameObject predatorFish;
+	//waypoint pos and rotation vectors, since only 2, easier to use seperate variables then arrays
+
+
+	// Use this for initialization
+	void Start () { //starts when set active, only once though
+		rocksExist = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void eatPlayer()
+	{
+		GameObject clone = Instantiate (predatorFish);
+	}
+}
