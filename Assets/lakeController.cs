34fce66cbec4cce0b1f287f9053a3236f139e543
Ryan Kramer <kramer545@gmail.com>
2017-10-02 16:17:20 -0700

    added 3D models of area indicators, unused for now, added touchscreen camera rotation

--- Assets/lakeController.cs
+++ Assets/lakeController.cs
@@ -23,15 +23,45 @@ public class lakeController : MonoBehaviour {
 	int endPosIndex;
 	int tempIndex = 0;
 
+	private Vector3 firstpoint;
+	private Vector3 secondpoint;
+	private float xAngle = 0.0f;
+	private float yAngle = 0.0f;
+	private float xAngTemp = 0.0f;
+	private float yAngTemp = 0.0f;
+
 
 
 	// Use this for initialization
 	void Start () {
 		endPosIndex = 0;
+		tempIndex = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+			//rotating camera w/ touchscreen
+			if(Input.touchCount > 0) {
+				//Touch began, save position
+				if(Input.GetTouch(0).phase == TouchPhase.Began) {
+					firstpoint = Input.GetTouch(0).position;
+					xAngTemp = xAngle;
+					yAngTemp = yAngle;
+				}
+				//Move finger by screen
+				if(Input.GetTouch(0).phase==TouchPhase.Moved) {
+					secondpoint = Input.GetTouch(0).position;
+					//Mainly, about rotate camera. For example, for Screen.width rotate on 180 degree
+					xAngle = xAngTemp + (secondpoint.x - firstpoint.x) * 180.0f / Screen.width;
+					yAngle = yAngTemp - (secondpoint.y - firstpoint.y) *90.0f / Screen.height;
+					//Rotate camera
+					if (yAngle > 90)//prevents camera flipping upside down
+						yAngle = 90;
+					else if (yAngle < -90)
+						yAngle = -90;
+				cams[tempIndex].transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+				}
+			}
 	}
 
 	public void areaClicked(int index)
@@ -51,19 +81,20 @@ public class lakeController : MonoBehaviour {
 		//Hide area indicators
 		//TODO replace this when replacing UI images with 3D models
 		if (tempIndex == 0) { //if index = 0, at base layer, show UI circles highlighting areas
-			setNormal ();
+			setNormal (); //reset underwater effects/fog 
 			foreach (GameObject img in overlayImages)
 				img.SetActive (true);
-		} else
-			setUnderWater ();
+		}
 
 		//activate areaPanel
 		if(tempIndex  != 0)
 			areaPanel[tempIndex ].SetActive(true);
 		//TODO put switch case or something here
 		//Activate relevant gameobjects for specific areas
-		if (tempIndex  == 1)
+		if (tempIndex  == 1) //trash island
 			garbageMinigame.SetActive (true);
+		if (tempIndex == 2) //docks
+			setUnderWater ();
 	}
 
 	public void garbageMinigameClick(int index)
