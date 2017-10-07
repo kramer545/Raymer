0547fc221b0406ed71bcf51d065d1f46f2fcca7d
Ryan Kramer <kramer545@gmail.com>
2017-10-07 15:07:41 -0700

    fixed certain menu buttons in main menu overlapping with icons to have endless loop of opening and closing, added text to intro tap me to start, other fixs

--- Assets/SolutionCatchMouse.cs
+++ Assets/SolutionCatchMouse.cs
@@ -5,20 +5,60 @@ using UnityEngine;
 public class SolutionCatchMouse : MonoBehaviour {
 	public GameObject solutionText;
 	public GameObject[] OtherTexts;
+	public GameObject[] Icons;
+	public bool iconsOn;
+	public bool isActive;
+	bool activateText;
 	// Use this for initialization
 	void Start () {
-		
+		iconsOn = false;
+		isActive = true;
+		activateText = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	void LateUpdate() //to prevent closing menu and hitting button at once, enable icons at end of fram
+	{
+		if(iconsOn)
+		{
+			iconsOn = false;
+			onClose ();
+		}
+
+		if(activateText)
+		{
+			solutionText.SetActive(true);
+			activateText = false;
+		}
+	}
 	
 	void OnMouseDown(){
-		foreach(GameObject text in OtherTexts){
-			text.SetActive(false);
+		if(isActive)
+		{
+			foreach(GameObject text in OtherTexts){
+				text.SetActive(false);
+			}
+			activateText = true;
+
+			foreach(GameObject icon in Icons){ //hide icons to prevent opening their menus when closing one
+				icon.GetComponent<SolutionCatchMouse> ().isActive = false;
+			}
 		}
-		solutionText.SetActive(true);
+	}
+
+	public void onClose() //reenables icons
+	{
+		foreach(GameObject icon in Icons){
+			icon.GetComponent<SolutionCatchMouse> ().isActive = true;
+		}
+	}
+
+	public void startIcons()
+	{
+		iconsOn = true;
 	}
 }
