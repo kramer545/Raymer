e68b7e1d09c06a5e108e62c6c0677c9ef953d31f
Ryan Kramer <kramer545@gmail.com>
2017-12-16 19:53:09 -0800

    added framework for polluton and tap areas, added fence/ditch to riparian area

---
+++ Assets/lakeScene/TapController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapController : MonoBehaviour {
+
+	public GameObject panelOne;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//base.rotate();
+	}
+
+	public void hidePanelOne()
+	{
+		panelOne.SetActive (false);
+	}
+}
