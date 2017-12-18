65c3b1fe814c4d005a6b7d1714af68c8ba7a6815
Ryan Kramer <kramer545@gmail.com>
2017-12-17 17:48:58 -0800

    added test fish movement cameras

---
+++ Assets/lakeScene/fishCam2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishCam2 : rotateCam {
+
+	public GameObject cam;
+	public float moveSpeed;
+	bool moving;
+
+	// Use this for initialization
+	void Start () {
+		moving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moving)
+			cam.transform.Translate (Vector3.forward * moveSpeed);
+		if (Input.GetKeyDown("up")) {
+			moving = !moving;
+		}
+	}
+}
