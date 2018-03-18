788b6cb84dbff5b4b7853b59dba7220f5f68c08c
Ryan Kramer <kramer545@gmail.com>
2018-03-18 09:32:19 -0700

    added touch screen controls, fixed terrains to match default w/ their properties

---
+++ Assets/lakeMoveBtns.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lakeMoveBtns : MonoBehaviour {
+
+	public GameObject cam;
+	public float moveSpeed;
+	int dir = -1;
+	float yPos;
+
+	// Use this for initialization
+	void Start () {
+		yPos = cam.transform.position.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (rotateCam.isActive && dir > -1) {
+			move ();
+		}
+	}
+
+	public void btnDown(int dir)
+	{
+		this.dir = dir;
+	}
+
+	public void btnUp()
+	{
+		dir = -1;
+	}
+
+	void move() {
+		if(dir == 0)
+		{
+			cam.transform.Translate (Vector3.forward * moveSpeed);
+		}
+		else if (dir == 1)
+		{
+			cam.transform.Translate (Vector3.forward * moveSpeed * -1);
+		}
+		else if (dir == 2)
+		{
+			cam.transform.Translate (Vector3.right * moveSpeed * -1);
+		}
+		else if (dir == 3)
+		{
+			cam.transform.Translate (Vector3.right * moveSpeed);
+		}
+		cam.transform.position = new Vector3 (cam.transform.position.x, yPos, cam.transform.position.z);
+	}
+}
